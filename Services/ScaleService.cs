7cf03059297fec4ff1121417ca6c9bce5c4a205c
igor egorov <igor127@bk.ru>
2022-10-30 19:59:00 +0300

    new xaml

---
+++ Services/ScaleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDrawObjects.Services
+{
+    public class ScaleService
+    {
+        private int _scale = 20;
+
+        public double Scale(double number)
+        {
+            return number * _scale;
+        }
+    }
+}
